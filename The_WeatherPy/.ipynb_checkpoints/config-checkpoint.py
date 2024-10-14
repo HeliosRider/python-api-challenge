fb707484a422b6dea058e6c7d0eb28d2c9695bac
Jonathan Calloway <stscsrtrd@gmail.com>
2024-10-14 16:31:43 -0400

    Created a README file

---
+++ The_WeatherPy/.ipynb_checkpoints/config-checkpoint.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+Weather_api_key = "11c952a211483b3674a1fa61d31f0919"
+GEO_api_key = "459a371d8aa5418b929223df24c32daa"
+
+
---
+++ The_WeatherPy/config.py
@@ -0,0 +1,6 @@
+# API Key
+Weather_api_key = "11c952a211483b3674a1fa61d31f0919"
+GEO_api_key = "459a371d8aa5418b929223df24c32daa"
+Vacation_api_key = "faecce2840954ead940f4c0a94091d75"
+
+
